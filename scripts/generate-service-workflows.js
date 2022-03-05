d1e18bf544b82edcae3491895c04416622611635
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-03-05 13:43:32 +0200

    WIP

--- scripts/generate-service-workflows.js
+++ scripts/generate-service-workflows.js
@@ -1,15 +1,15 @@
 const path = require('path')
 const fs = require('fs')
 
-const template = fs.readFileSync(path.join(__dirname, 'service.template.yaml'), 'utf8')
+const template = fs.readFileSync(path.join(__dirname, 'service.template.yml'), 'utf8')
 const services = fs.readdirSync(path.join(__dirname, '..', 'services'), 'utf8')
 
-const githubDir = path.join(__dirname, '..', '.github')
+const workflowDir = path.join(__dirname, '..', '.github', 'workflows')
 
-if (!fs.existsSync(githubDir)) fs.mkdirSync(githubDir)
+if (!fs.existsSync(workflowDir)) fs.mkdirSync(workflowDir, { recursive: true })
 
 for (const service of services) {
   console.log(`Generating workflow for ${service}`)
   const workflow = template.replace(/{{SERVICE}}/g, service)
-  fs.writeFileSync(path.join(githubDir, `${service}.yaml`), workflow)
+  fs.writeFileSync(path.join(workflowDir, `${service}.yml`), workflow)
 }
